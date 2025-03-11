1deca662037dfa9393c64f3cea0ccf6053a42d9c
adamal.sall <115763652+Kaiyfa@users.noreply.github.com>
2025-03-11 10:23:32 +0300

    Fixed Procfile path for Render deployment

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -14,6 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
+os.environ.setdefault("PORT", "8000") 
 
 
 if __name__ == "__main__":
