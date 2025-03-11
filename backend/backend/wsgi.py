c86e69a6e8228a2e6270d64ca9275b273f0e4b78
adamal.sall <115763652+Kaiyfa@users.noreply.github.com>
2025-03-11 08:30:15 +0300

    update wsgi.py

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -14,3 +14,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
+
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    os.environ.setdefault("PORT", "8000")
+    execute_from_command_line(["manage.py", "runserver", "0.0.0.0:8000"])
